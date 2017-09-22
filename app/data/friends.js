153805602ed17bd008d462128f7a54da9da0660a
Ben Sojka <bensojka@gmail.com>
2017-09-22 15:44:07 -0500

    9/22

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+// Friends (DATA)
+// =============================================================
+
+var friends = [
+    {
+    name:"Ben S",
+    photo:"https://scontent.fmkc1-1.fna.fbcdn.net/v/t31.0-8/20776597_4512917419738_218009932475581939_o.jpg?oh=a4cc134c89a960287442284b8ce31772&oe=5A877DAF",
+    scores:[
+        5,
+        5,
+        5,
+        5,
+        1,
+        5,
+        5,
+        5,
+        1,
+        1
+    ]
+    },
+    {
+    name:"Cait S",
+    photo:"https://scontent.fmkc1-1.fna.fbcdn.net/v/t1.0-9/18620472_1350414598368206_3107937465015911900_n.jpg?oh=fb2ce649b97369169cf1101c85b576f0&oe=5A52F5E2",
+    scores:[
+        1,
+        4,
+        1,
+        3,
+        1,
+        2,
+        1,
+        5,
+        4,
+        1
+    ]
+    }
+
+];
+
+module.exports = friends;
