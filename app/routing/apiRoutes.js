fdd6e532ec3fcc67fcc5362e1f1a888981316028
Ben Sojka <bensojka@gmail.com>
2017-10-05 12:05:01 -0500

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,30 +19,27 @@ module.exports = function(app) {
 		var usrName 	= usrData.name;
 		var usrImage 	= usrData.image;
 		var usrScores 	= usrData.scores;
-
-		var totalDifference = 0;
-
+		
 		//loop through the friends data array of objects to get each friends scores
-		for(var i = 0; i < [friends].length-1; i++){
-			console.log(friends[i].name);
-			totalDifference = 0;
+		for ( var i = 0; i < friends.length; i++ ) {
+			var totalDifference = 0;
 
 			//loop through that friends score and the users score and calculate the 
 			// absolute difference between the two and push that to the total difference variable set above
-			for(var j = 0; j < 10; j++){
+			for ( var j = 0; j < 10; j++ ){
 				// We calculate the difference between the scores and sum them into the totalDifference
 				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
 				// If the sum of differences is less then the differences of the current "best match"
-				if (totalDifference <= greatMatch.friendDifference){
+			}
 
-					// Reset the bestMatch to be the new friend. 
-					greatMatch.name = friends[i].name;
-					greatMatch.photo = friends[i].photo;
-					greatMatch.matchDifference = totalDifference;
-				}
+			if ( totalDifference <= greatMatch.matchDifference ) {
+				// Reset the bestMatch to be the new friend. 
+				greatMatch.name = friends[i].name;
+				greatMatch.photo = friends[i].photo;
+				greatMatch.matchDifference = totalDifference;
 			}
 		}
-
+		
 		friends.push(usrData);
  
 		res.json(greatMatch); 
